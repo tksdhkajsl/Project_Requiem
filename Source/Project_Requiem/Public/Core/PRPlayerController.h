f1444387e758dae7966c60b25bd09585eebe758a
cheonsooho <sky_defense@naver.com>
2025-12-11 18:45:24 +0900

    Connect Boss HUD
    1. Added BossDoor
    2. Changed HUD

--- Source/Project_Requiem/Public/Core/PRPlayerController.h
+++ Source/Project_Requiem/Public/Core/PRPlayerController.h
@@ -5,6 +5,7 @@
 #include "PRPlayerController.generated.h"
 
 class UPRHUDWidget;
+class ABaseCharacter;
 
 UCLASS()
 class PROJECT_REQUIEM_API APRPlayerController : public APlayerController
@@ -17,16 +18,9 @@ public:
 	APRPlayerController();
 protected:
 	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
 #pragma endregion
 
-	/// Todo : 플레이어 할 행동
-	/*
-		if (APRPlayerController* PC = GetController<APRPlayerController>()) {
-		PC->PushDownKeyboard1~3();	 // 키 바인딩해야함
-		// 또는
-		if (APRPlayerController* PC = GetController<APRPlayerController>()) {
-		PC->PushDownKeyboard1~4(int32 포션갯수);	 // 키 바인딩해야함
-	*/
 #pragma region HUD 위젯 클래스
 public:
 	// HUD 위젯 클래스 (언리얼 에디터에서 Blueprint HUD 클래스를 지정하기 위해 사용)
@@ -48,6 +42,17 @@ public:
 	/** 포션 용 키 바인딩 */
 	UFUNCTION()
 	void PushDownKeyboard4(int32 PotionNum);
+#pragma endregion
+
+#pragma region 보스 관련 HUD 갱신
+public:
+	UFUNCTION()
+	void OnEnterBossRoom(ABaseCharacter* Boss);
+	UFUNCTION()
+	void HandleBossHPChanged(EFullStats StatType, float Curr, float Max);
+
+	UPROPERTY(VisibleAnywhere, Transient, Category = "Boss Data")
+	ABaseCharacter* ActiveBoss = nullptr;
 #pragma endregion
 
 };
