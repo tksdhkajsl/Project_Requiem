2cd90073c20286d1b333e874e60900d8c2274a95
Yong770 <qkrdyddyd7@naver.com>
2025-12-11 10:10:34 +0900

    BossBase 폴더구조변경

--- Source/Project_Requiem/Private/BossBase/BossBase.cpp
+++ Source/Project_Requiem/Private/BossBase/BossBase.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "BossBase.h"
+#include "Bossbase/BossBase.h"
 #include "Kismet/GameplayStatics.h"
 #include "GameFramework/CharacterMovementComponent.h"
 
